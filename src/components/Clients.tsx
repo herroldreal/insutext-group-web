bc7254cdd4d5ec3cb6c56bc8fd57577d3500bc25
Herrold H. Real <realherrold@gmail.com>
2024-11-11 17:12:03 -0600

    Changing text, images, marketing tags, metadata for InsuTex Group

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -13,7 +13,7 @@ const Clients: React.FC = () => {
                     <div className="col-lg-3 col-md-4 pq-border-right">
                         <div className="pq-counter pq-counter-default">
                             <div className="pq-counter-num-prefix">
-                                <h5 className="timer" data-to="6050" data-speed="5000">11</h5>
+                                <h5 className="timer" data-to="11" data-speed="5000">11</h5>
                             </div>
                             <span className="pq-counter-title">Nuestras marcas</span>
                         </div>
@@ -21,8 +21,8 @@ const Clients: React.FC = () => {
                     <div className="col-lg-9 col-md-8 mt-4 mt-md-0 pq-ps-90">
                         <div className="pq-client-box pq-client-style-1">
                             <div className="owl-carousel owl-loaded owl-drag" data-dots="false" data-nav="false"
-                                 data-desk_num="5" data-lap_num="3" data-tab_num="2" data-mob_num="2" data-mob_sm="2"
-                                 data-autoplay="false" data-loop="true" data-margin="30">
+                                 data-desk_num="6" data-lap_num="8" data-tab_num="3" data-mob_num="3" data-mob_sm="3"
+                                 data-autoplay="true" data-loop="true" data-margin="25">
                                 <div className="item">
                                     <div className="pq-client-box pq-style-1">
                                         <div>
@@ -46,9 +46,9 @@ const Clients: React.FC = () => {
                                 <div className="item">
                                     <div className="pq-client-box pq-style-1">
                                         <div>
-                                            <img decoding="async" src="/images/client/slider/3.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/3.svg" alt="pq-client-img"
                                                  className="pq-client-img"/>
-                                            <img decoding="async" src="/images/client/slider/h3.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/h3.svg" alt="pq-client-img"
                                                  className="pq-client-hover-img"/>
                                         </div>
                                     </div>
@@ -56,9 +56,9 @@ const Clients: React.FC = () => {
                                 <div className="item">
                                     <div className="pq-client-box pq-style-1">
                                         <div>
-                                            <img decoding="async" src="/images/client/slider/4.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/4.svg" alt="pq-client-img"
                                                  className="pq-client-img"/>
-                                            <img decoding="async" src="/images/client/slider/h4.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/h4.svg" alt="pq-client-img"
                                                  className="pq-client-hover-img"/>
                                         </div>
                                     </div>
@@ -66,9 +66,9 @@ const Clients: React.FC = () => {
                                 <div className="item">
                                     <div className="pq-client-box pq-style-1">
                                         <div>
-                                            <img decoding="async" src="/images/client/slider/5.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/5.svg" alt="pq-client-img"
                                                  className="pq-client-img"/>
-                                            <img decoding="async" src="/images/client/slider/h5.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/h5.svg" alt="pq-client-img"
                                                  className="pq-client-hover-img"/>
                                         </div>
                                     </div>
@@ -76,9 +76,69 @@ const Clients: React.FC = () => {
                                 <div className="item">
                                     <div className="pq-client-box pq-style-1">
                                         <div>
-                                            <img decoding="async" src="/images/client/slider/6.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/6.svg" alt="pq-client-img"
                                                  className="pq-client-img"/>
-                                            <img decoding="async" src="/images/client/slider/h6.png" alt="pq-client-img"
+                                            <img decoding="async" src="/images/client/slider/h6.svg" alt="pq-client-img"
+                                                 className="pq-client-hover-img"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="pq-client-box pq-style-1">
+                                        <div>
+                                            <img decoding="async" src="/images/client/slider/7.svg" alt="pq-client-img"
+                                                 className="pq-client-img"/>
+                                            <img decoding="async" src="/images/client/slider/h7.svg" alt="pq-client-img"
+                                                 className="pq-client-hover-img"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="pq-client-box pq-style-1">
+                                        <div>
+                                            <img decoding="async" src="/images/client/slider/1.svg" alt="pq-client-img"
+                                                 className="pq-client-img"/>
+                                            <img decoding="async" src="/images/client/slider/h1.svg" alt="pq-client-img"
+                                                 className="pq-client-hover-img"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="pq-client-box pq-style-1">
+                                        <div>
+                                            <img decoding="async" src="/images/client/slider/2.svg" alt="pq-client-img"
+                                                 className="pq-client-img"/>
+                                            <img decoding="async" src="/images/client/slider/h2.svg" alt="pq-client-img"
+                                                 className="pq-client-hover-img"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="pq-client-box pq-style-1">
+                                        <div>
+                                            <img decoding="async" src="/images/client/slider/3.svg" alt="pq-client-img"
+                                                 className="pq-client-img"/>
+                                            <img decoding="async" src="/images/client/slider/h3.svg" alt="pq-client-img"
+                                                 className="pq-client-hover-img"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="pq-client-box pq-style-1">
+                                        <div>
+                                            <img decoding="async" src="/images/client/slider/4.svg" alt="pq-client-img"
+                                                 className="pq-client-img"/>
+                                            <img decoding="async" src="/images/client/slider/h4.svg" alt="pq-client-img"
+                                                 className="pq-client-hover-img"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="pq-client-box pq-style-1">
+                                        <div>
+                                            <img decoding="async" src="/images/client/slider/6.svg" alt="pq-client-img"
+                                                 className="pq-client-img"/>
+                                            <img decoding="async" src="/images/client/slider/h6.svg" alt="pq-client-img"
                                                  className="pq-client-hover-img"/>
                                         </div>
                                     </div>
